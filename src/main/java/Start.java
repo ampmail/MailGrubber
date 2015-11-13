3c53e7bd38b38bff5ec99e7c88001b365892b396
ampuser <ampmail@mail.ru>
2015-11-13 19:41:05 +0200

    2

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -7,14 +7,14 @@ import dclink.multilang.MultiLang;
  */
 public class Start {
     public static void main(String[] args) {
-        System.out.println(MultiLang.getMsg("welcome"));
-        MultiLang.setCurrentLocale(MultiLang.Lang.UA);
-        System.out.println(MultiLang.getMsg("welcome"));
-        System.out.println(MultiLang.getMsgByLang(MultiLang.Lang.RU, "welcome"));
-        System.out.println(MultiLang.getMsgByLang(MultiLang.Lang.EN, "welcome"));
+//        System.out.println(MultiLang.getMsg("welcome"));
+//        MultiLang.setCurrentLocale(MultiLang.Lang.UA);
+//        System.out.println(MultiLang.getMsg("welcome"));
+//        System.out.println(MultiLang.getMsgByLang(MultiLang.Lang.RU, "welcome"));
+//        System.out.println(MultiLang.getMsgByLang(MultiLang.Lang.EN, "welcome"));
 
-        OpenXLS xls = new OpenXLS("price-07-11-15.xlsx");
-        xls.setActiveWorkSheetByName("TDSheet");
+        OpenXLS xls = new OpenXLS("price-10-11-15.xlsx");
+        xls.setActiveWorkSheetByName(null);
         xls.getWorkSheetData();
 
     }
--- src/main/java/dclink/excel/OpenXLS.java
+++ src/main/java/dclink/excel/OpenXLS.java
@@ -5,13 +5,16 @@ import com.extentech.ExtenXLS.RowHandle;
 import com.extentech.ExtenXLS.WorkBookHandle;
 import com.extentech.ExtenXLS.WorkSheetHandle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ampuser on 06.11.2015.
  */
 public class OpenXLS {
 
-//    String workingdir = "D:\\Temp\\";
-    private final String workingdir = "c:\\Users\\ampuser\\Downloads\\";
+    private final String workingdir = "D:\\Temp\\1\\";
+//    private final String workingdir = "c:\\Users\\ampuser\\Downloads\\";
     private WorkBookHandle workBookHandle;
     private WorkSheetHandle workSheet;
 
@@ -20,20 +23,42 @@ public class OpenXLS {
     }
 
     public OpenXLS(String fileName){
-        getWorkBook(fileName);
+        if(fileName != null) {
+            getWorkBook(fileName);
+        }
     }
 
     public void getWorkBook (String fileName) {
         workBookHandle = new WorkBookHandle(workingdir + fileName);
     }
 
-    public void setActiveWorkSheetByName (String sheetname){
-        try {
-            workSheet = workBookHandle.getWorkSheet(sheetname);
+    public List<String> getWorkBookSheetsNames () {
+        List<String> sheetNamesList = new ArrayList<>();
+        if(workBookHandle != null) {
+//            sheetNamesList = workBookHandle.getWorkSheets();
+        }
+        return sheetNamesList;
+    }
+
+    public void setActiveWorkSheetByName (Object sheetname){
+        if(workBookHandle != null) {
+            try {
+                if (sheetname.getClass().equals(new String().getClass())) {
+                    workSheet = workBookHandle.getWorkSheet((String) sheetname);
+                }
+                if (sheetname.getClass().equals(new Integer(0).getClass())) {
+                    workSheet = workBookHandle.getWorkSheet((Integer) sheetname);
+                }
+                if (sheetname == null) {
+                    System.out.println(1);
+                    workBookHandle = workBookHandle.getNoSheetWorkBook();
+                    workSheet = workBookHandle.getActiveSheet();
+                }
 //            System.out.println(workSheet.getFirstCol() + "\t" + workSheet.getFirstRow());
 //            System.out.println(workSheet.getLastCol() + "\t" + workSheet.getLastRow());
-        }catch (Exception workSheetHandle){
-            System.out.println(workSheetHandle);
+            } catch (Exception workSheetHandle) {
+                System.out.println(workSheetHandle);
+            }
         }
     }
 
